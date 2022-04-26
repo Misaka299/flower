54663e79f3719214e78c6e263aa54b6f18421b8a
Misaka15535 <46892084+Misaka15535@users.noreply.github.com>
2022-04-26 19:36:44 +0800

    图片加载不正确

--- flower/src/controls/button.rs
+++ flower/src/controls/button.rs
@@ -1,7 +1,7 @@
 use std::ops::{Deref, DerefMut};
-use glow::HasContext;
 
-use log::debug;
+use glow::{HasContext, LINEAR, NEAREST, REPEAT, RGB, RGBA, TEXTURE_2D, TEXTURE_MAG_FILTER, TEXTURE_MIN_FILTER, TEXTURE_WRAP_S, TEXTURE_WRAP_T, UNSIGNED_BYTE};
+use image::GenericImageView;
 
 use crate::color::Color;
 use crate::control::{Control, ControlState, ControlType, InteractiveState};
@@ -51,26 +51,53 @@ impl DerefMut for Button {
 
 impl Control for Button {
     fn on_draw(&mut self, gl: &mut Draw) {
-        println!("button[{}] draw rect {:?}",self.id(),&self.rect);
-        // gl.create_canvas(&Rect::new(self.base_left + self.left,self.base_top + self.top, self.width, self.height));
+        println!("button[{}] draw rect {:?}", self.id(), &self.rect);
+        gl.create_canvas(&Rect::new(self.base_left + self.left,self.base_top + self.top, self.width, self.height));
         unsafe {
-            // gl.clear_color(0.1, 0.2, 0.3, 1.0);
+            // gl.clear_color(1.0, 1.0, 1.0, 1.0);
             // gl.clear(glow::COLOR_BUFFER_BIT);
+
+
+            let texture = gl.create_texture().unwrap();
+            gl.bind_texture(TEXTURE_2D, Some(texture));
+
+            // 为当前绑定的纹理对象设置环绕、过滤方式
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_WRAP_S, REPEAT as i32);
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_WRAP_T, REPEAT as i32);
+            //
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR as i32);
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MAG_FILTER, LINEAR as i32);
+
+
+            // 加载并生成纹理
+            let image = image::open("flower/resource/test.jpg").expect("Failed to load texture");
+            let data = image.as_bytes();
+
+
+            gl.tex_image_2d(TEXTURE_2D, 0, RGB as i32, image.width() as i32, image.height() as i32, 0, RGB, UNSIGNED_BYTE, Some(data));
+
+            gl.generate_mipmap(TEXTURE_2D);
+            let i = gl.get_error();
+            println!("-----  {}",i);
+            for x in gl.get_debug_message_log(i) {
+                println!("x---- {:?}",x);
+            }
         }
-        println!("button[{}] focus {}",self.id(), self.focus);
-        match self.interactive_state {
-            InteractiveState::Ordinary => {
-                gl.fill(&self.rect, &Color::from_hex_str("#FFF").unwrap());
-            }
-            InteractiveState::Active => {
-                gl.fill(&self.rect, &Color::from_hex_str("#efefef").unwrap());
-            }
-            InteractiveState::Pressed => {
-                gl.fill(&self.rect, &Color::from_hex_str("#3c4043").unwrap());
-            }
-            InteractiveState::Disable => {
-                gl.fill(&self.rect, &Color::from_hex_str("##eaeaea").unwrap());
-            }
-        }
+        println!("button[{}] focus {}", self.id(), self.focus);
+
+        // match self.interactive_state {
+        //     InteractiveState::Ordinary => {
+        //         gl.fill(&self.rect, &Color::from_hex_str("#FFF").unwrap());
+        //     }
+        //     InteractiveState::Active => {
+        //         gl.fill(&self.rect, &Color::from_hex_str("#efefef").unwrap());
+        //     }
+        //     InteractiveState::Pressed => {
+        //         gl.fill(&self.rect, &Color::from_hex_str("#3c4043").unwrap());
+        //     }
+        //     InteractiveState::Disable => {
+        //         gl.fill(&self.rect, &Color::from_hex_str("##eaeaea").unwrap());
+        //     }
+        // }
     }
 }
